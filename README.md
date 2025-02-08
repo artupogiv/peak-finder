b1d03cca6ea2932a2492b800dd5a74cd16756dcc
Vigo Putra <artupogiv@gmail.com>
2025-02-08 15:19:37 +0700

    feat: implement initial endpoints

--- README.md
+++ README.md
@@ -4,24 +4,26 @@ REST API to explore mounts information.
 
 ## REST API Specification
 
-| Endpoint Path   | HTTP Method   | Description                              |
-| --------------- | ------------- | ---------------------------------------- |
-| `/mounts`       | `GET`         | List all mounts                          |
-| `/mounts/:id`   | `GET`         | Get mount by id                          |
-| `/mounts`       | `POST`        | Create new mount                         |
-| `/mounts`       | `DELETE`      | Delete all mounts                        |
-| `/mounts/:id`   | `DELETE`      | Delete mount by id                       |
-| `/mounts/:id`   | `PATCH`       | Update mount by id                       |
-| `/mounts/:id`   | `PUT`         | Update mount by id, create if not exist  |
+| Endpoint Path | HTTP Method | Description                             |
+| ------------- | ----------- | --------------------------------------- |
+| `/mounts`     | `GET`       | List all mounts                         |
+| `/mounts/:id` | `GET`       | Get mount by id                         |
+| `/mounts`     | `POST`      | Create new mount                        |
+| `/mounts`     | `DELETE`    | Delete all mounts                       |
+| `/mounts/:id` | `DELETE`    | Delete mount by id                      |
+| `/mounts/:id` | `PATCH`     | Update mount by id                      |
+| `/mounts/:id` | `PUT`       | Update mount by id, create if not exist |
 
 ## Get Started
 
 To install dependencies:
+
 ```sh
 bun install
 ```
 
 To run:
+
 ```sh
 bun dev
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,92 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type Mount = {
+  id: number;
+  name: string;
+  island: string;
+  province: string;
+  elevation: number;
+  status?: string; // active or not active
+};
 
-export default app
+let mounts: Mount[] = [
+  {
+    id: 1,
+    name: "Gunung Kerinci",
+    island: "Sumatera",
+    province: "Sumatera Barat",
+    elevation: 3805,
+  },
+  {
+    id: 2,
+    name: "Gunung Sibayak",
+    island: "Sumatera",
+    province: "Sumatera Utara",
+    elevation: 2212,
+  },
+  {
+    id: 3,
+    name: "Gunung Dempo",
+    island: "Sumatera",
+    province: "Sumatera Selatan",
+    elevation: 3159,
+  },
+  {
+    id: 4,
+    name: "Gunung Leuser",
+    island: "Sumatera",
+    province: "Aceh",
+    elevation: 3404,
+  },
+  {
+    id: 5,
+    name: "Gunung Slamet",
+    island: "Jawa",
+    province: "Jawa Tengah",
+    elevation: 3428,
+  },
+  {
+    id: 6,
+    name: "Gunung Semeru",
+    island: "Jawa",
+    province: "Jawa Timur",
+    elevation: 3676,
+  },
+  {
+    id: 7,
+    name: "Gunung Gede",
+    island: "Jawa",
+    province: "Jawa Barat",
+    elevation: 2958,
+  },
+];
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello World!",
+  });
+});
+
+// GET /mounts
+app.get("/mounts", (c) => {
+  return c.json(mounts);
+});
+
+// GET /mounts/:id
+app.get("/mounts/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  const mount = mounts.find((mount) => {
+    return mount.id === id;
+  });
+
+  if (!mount) {
+    return c.json({ message: "Mount not found" }, 404);
+  }
+
+  return c.json(mount);
+});
+
+export default app;
