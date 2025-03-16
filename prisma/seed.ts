87b189c15ef77480131dc261960b11e50f111615
Vigo Putra <artupogiv@gmail.com>
2025-03-16 07:04:01 +0700

    feat: add seeding islands and provinces

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,26 +1,35 @@
 import { PrismaClient } from "@prisma/client";
-import { sample} from "underscore";
+import { sample } from "underscore";
 import { islands, provinces, mountains } from "./data";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const resultIslands = await prisma.island.createManyAndReturn({
-    data: islands,
-    select: {
-      id: true,
-    },
-  });
-  console.log("Island data created");
+  for (const island of islands) {
+    const resultIslands = await prisma.island.upsert({
+      where: {
+        slug: island.slug,
+      },
+      update: island,
+      create: island,
+    });
 
-  const resultProvinces = await prisma.province.createManyAndReturn({
-    data: provinces,
-    select: {
-      id: true,
-    },
-  });
-  console.log("Province data created");
+    console.log(`Island data created`);
+  }
 
+  for (const province of provinces) {
+    const resultProvinces = await prisma.province.upsert({
+      where: {
+        slug: province.slug,
+      },
+      update: province,
+      create: province,
+    });
+
+    console.log(`Province data created`);
+  }
+
+  
   for (const mountain of mountains) {
     await prisma.mountain.create({
       data: {
@@ -35,7 +44,6 @@ async function main() {
     });
 
     console.log(`Mountains data created`);
-  }
 }
 
 main()
