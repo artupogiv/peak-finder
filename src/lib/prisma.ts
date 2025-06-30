0093edaf46e1ae04ff8e795a82f6c8b1123032b6
Vigo Putra <artupogiv@gmail.com>
2025-07-01 00:56:28 +0700

    deps: update prisma & add prisma generator

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 
 export const prisma = new PrismaClient({
-  log: ["info","error","query"],
+  log: ["info", "error", "query"],
 });
